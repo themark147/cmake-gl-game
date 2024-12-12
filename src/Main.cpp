a80ad56b9073fcf804fbe2e60347cf52f2a9de6d
Marek Šuca <mark.suca@gmail.com>
2024-12-12 21:09:33 +0100

    key with toggle functionality

--- src/Main.cpp
+++ src/Main.cpp
@@ -24,6 +24,22 @@ void processInput(GLFWwindow* window);
 const unsigned int SCR_WIDTH = 1920;
 const unsigned int SCR_HEIGHT = 1080;
 
+enum KeyDefinition {
+    KEY_G = 0,
+    KEY_H = 1,
+    KEY_T = 2
+};
+
+struct Key {
+    int keyCode;
+    bool state;
+    int previousState;
+
+    Key(int key) : keyCode(key), state(false), previousState(GLFW_RELEASE) {}
+};
+
+std::vector<Key> keys;
+
 Camera camera(glm::vec3(0.0f, 0.0f, 15.0f));
 Object firstBox(glm::vec3(0.0f, 0.0f, 15.0f));
 
@@ -55,11 +71,28 @@ int main()
         return -1;
     }
 
+    // It has to be in same order as KeyDefinition
+    keys.push_back(Key(GLFW_KEY_G));
+    keys.push_back(Key(GLFW_KEY_H));
+    keys.push_back(Key(GLFW_KEY_T));
+
     while (!glfwWindowShouldClose(window))
     {
         processInput(window);
 
-        glClearColor(1.0f, 1.0f, 0.0f, 1.0f);
+        if (keys[KeyDefinition::KEY_T].state) {
+            glClearColor(1.0f, 0.0f, 0.0f, 1.0f); // 'T' key - Red
+        }
+        else if (keys[KeyDefinition::KEY_G].state) {
+            glClearColor(0.0f, 1.0f, 0.0f, 1.0f); // 'G' key - Green
+        }
+        else if (keys[KeyDefinition::KEY_H].state) {
+            glClearColor(0.0f, 0.0f, 1.0f, 1.0f); // 'H' key - Blue
+        }
+        else {
+            glClearColor(0.0f, 0.0f, 0.0f, 1.0f); // Default - Black
+        }
+
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
         glfwSwapBuffers(window);
@@ -75,6 +108,18 @@ void processInput(GLFWwindow* window)
 {
     if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
         glfwSetWindowShouldClose(window, true);
+
+    for (auto& key : keys) {
+        int keyState = glfwGetKey(window, key.keyCode);
+
+        if (keyState == GLFW_PRESS && key.previousState == GLFW_RELEASE) {
+            key.state = !key.state;
+            printf("Key %d toggled to %s\n", key.keyCode, key.state ? "ON" : "OFF");
+
+        }
+
+        key.previousState = keyState;
+    }  
 }
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
