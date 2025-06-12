b92415779c86c03c371db74a32b36c84b3ad2755
Matuzalllem <mark.suca@gmail.com>
2025-06-12 22:32:09 +0200

    new shader helped w/ little shadow triangles

---
+++ shaders/mainWShadow.fs
@@ -0,0 +1,158 @@
+#version 330 core
+
+in vec3 FragPos;
+in vec2 TexCoords;
+in vec3 Normal;
+in vec3 Tangent;
+in vec3 Bitangent;
+in vec4 FragPosLightSpace;
+
+out vec4 FragColor;
+
+uniform sampler2D shadowMap;
+
+struct Material {
+    sampler2D diffuse; // albedo
+    sampler2D normal;
+}; 
+
+// Camera position
+uniform vec3 viewPos;
+
+// Light information
+uniform vec3 lightPos;
+uniform vec3 lightColor;
+uniform vec3 lightDir;
+
+uniform Material material;
+
+const float PI = 3.14159265359;
+
+float ShadowCalculation(vec4 fragPosLightSpace)
+{
+    // perform perspective divide
+    vec3 projCoords = fragPosLightSpace.xyz / fragPosLightSpace.w;
+    // transform to [0,1] range
+    projCoords = projCoords * 0.5 + 0.5;
+    // get closest depth value from light's perspective (using [0,1] range fragPosLight as coords)
+    float closestDepth = texture(shadowMap, projCoords.xy).r; 
+    // get depth of current fragment from light's perspective
+    float currentDepth = projCoords.z;
+    // calculate bias (based on depth map resolution and slope)
+    vec3 normal = normalize(Normal);
+    vec3 lightDir = normalize(lightPos - FragPos);
+    float bias = max(0.05 * (1.0 - dot(normal, lightDir)), 0.005);
+    // check whether current frag pos is in shadow
+    // float shadow = currentDepth - bias > closestDepth  ? 1.0 : 0.0;
+    // PCF
+    float shadow = 0.0;
+    vec2 texelSize = 1.0 / textureSize(shadowMap, 0);
+    for(int x = -1; x <= 1; ++x)
+    {
+        for(int y = -1; y <= 1; ++y)
+        {
+            float pcfDepth = texture(shadowMap, projCoords.xy + vec2(x, y) * texelSize).r; 
+            shadow += currentDepth - bias > pcfDepth  ? 1.0 : 0.0;        
+        }    
+    }
+    shadow /= 9.0;
+    
+    // keep the shadow at 0.0 when outside the far_plane region of the light's frustum.
+    if(projCoords.z > 1.0)
+        shadow = 0.0;
+        
+    return shadow;
+}
+
+// Function to calculate the normal from the normal map
+vec3 getNormalFromMap()
+{
+    vec3 tangentNormal = texture(material.normal, TexCoords).rgb * 2.0 - 1.0;
+
+    vec3 Q1  = dFdx(FragPos);
+    vec3 Q2  = dFdy(FragPos);
+    vec2 st1 = dFdx(TexCoords);
+    vec2 st2 = dFdy(TexCoords);
+
+    vec3 N   = normalize(Normal);
+    vec3 T   = normalize(Tangent);
+    vec3 B   = normalize(Bitangent);
+    mat3 TBN = mat3(T, B, N);
+
+    return normalize(TBN * tangentNormal);
+}
+
+// Normal Distribution Function (GGX/Trowbridge-Reitz)
+float DistributionGGX(vec3 N, vec3 H, float roughness)
+{
+    float a = roughness * roughness;
+    float a2 = a * a;
+    float NdotH = max(dot(N, H), 0.0);
+    float NdotH2 = NdotH * NdotH;
+
+    float nom   = a2;
+    float denom = (NdotH2 * (a2 - 1.0) + 1.0);
+    denom = PI * denom * denom;
+
+    return nom / denom;
+}
+
+// Geometry Function (Schlick GGX)
+float GeometrySchlickGGX(float NdotV, float roughness)
+{
+    float r = (roughness + 1.0);
+    float k = (r * r) / 8.0;
+
+    float nom   = NdotV;
+    float denom = NdotV * (1.0 - k) + k;
+
+    return nom / denom;
+}
+
+// Geometry Function (Smith)
+float GeometrySmith(vec3 N, vec3 V, vec3 L, float roughness)
+{
+    float NdotV = max(dot(N, V), 0.0);
+    float NdotL = max(dot(N, L), 0.0);
+    float ggx1 = GeometrySchlickGGX(NdotV, roughness);
+    float ggx2 = GeometrySchlickGGX(NdotL, roughness);
+
+    return ggx1 * ggx2;
+}
+
+// Fresnel Equation (Schlick approximation)
+vec3 fresnelSchlick(float cosTheta, vec3 F0)
+{
+    return F0 + (1.0 - F0) * pow(1.0 - cosTheta, 5.0);
+}
+
+vec3 sRGBToLinear(vec3 color)
+{
+    return pow(color, vec3(2.2));
+}
+
+void main()
+{
+    // Sample textures
+    vec3 color = texture(material.diffuse, TexCoords).rgb;
+    vec3 normal = normalize(Normal);
+    vec3 lightColor = vec3(0.3);
+    // ambient
+    vec3 ambient = 0.3 * lightColor;
+    // diffuse
+    vec3 lightDir = normalize(lightPos - FragPos);
+    float diff = max(dot(lightDir, normal), 0.0);
+    vec3 diffuse = diff * lightColor;
+    // specular
+    vec3 viewDir = normalize(viewPos - FragPos);
+    vec3 reflectDir = reflect(-lightDir, normal);
+    float spec = 0.0;
+    vec3 halfwayDir = normalize(lightDir + viewDir);  
+    spec = pow(max(dot(normal, halfwayDir), 0.0), 64.0);
+    vec3 specular = spec * lightColor;    
+    // calculate shadow
+    float shadow = ShadowCalculation(FragPosLightSpace);                      
+    vec3 lighting = (ambient + (1.0 - shadow) * (diffuse + specular)) * color;    
+    
+    FragColor = vec4(lighting, 1.0);
+}
---
+++ shaders/mainWShadow.vs
@@ -0,0 +1,49 @@
+#version 330 core
+
+layout(location = 0) in vec3 aPos;
+layout(location = 1) in vec3 aNormal;
+layout(location = 2) in vec2 aTexCoords;
+layout(location = 3) in vec3 aTangent;
+layout(location = 4) in vec3 aBitangent;
+
+layout(location = 5) in ivec4 boneIds;
+layout(location = 6) in vec4 boneWeights;
+
+out vec3 FragPos;
+out vec2 TexCoords;
+out vec3 Normal;
+out vec3 Tangent;
+out vec3 Bitangent;
+out vec4 FragPosLightSpace;
+
+uniform mat4 model;
+uniform mat4 view;
+uniform mat4 projection;
+uniform mat4 lightSpaceMatrix;
+
+uniform mat4 bone_transforms[100];
+out vec4 bw;
+
+void main()
+{
+    mat4 boneTransform  =  mat4(1.0);
+	boneTransform  +=    bone_transforms[int(boneIds.x)] * boneWeights.x;
+	boneTransform  +=    bone_transforms[int(boneIds.y)] * boneWeights.y;
+	boneTransform  +=    bone_transforms[int(boneIds.z)] * boneWeights.z;
+	boneTransform  +=    bone_transforms[int(boneIds.w)] * boneWeights.w;
+
+    vec4 pos = boneTransform * vec4(aPos, 1.0);
+    
+    FragPos = vec3(model * vec4(aPos, 1.0));
+    TexCoords = aTexCoords;
+
+    mat3 normalMatrix = transpose(inverse(mat3(model))) * mat3(boneTransform);
+
+    Normal = normalMatrix * aNormal;
+    Tangent = normalMatrix * aTangent;
+    Bitangent = normalMatrix * aBitangent;
+
+    FragPosLightSpace = lightSpaceMatrix * vec4(FragPos, 1.0);
+    
+    gl_Position = projection * view * model * pos;
+}
